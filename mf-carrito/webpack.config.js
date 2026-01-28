47bee2aead97800d3f1244420e7dd7e9d9d4e5ca
Xavier Navia <xaviedunavi@gmail.com>
2026-01-27 20:08:00 -0500

    Track mf-carrito as regular folder

---
+++ mf-carrito/webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    port: 3003,
+    open: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfCarrito',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Carrito': './src/components/Carrito',
+      },
+      shared: {
+        react: { singleton: true, eager: true },
+        'react-dom': { singleton: true, eager: true },
+        'styled-components': { singleton: true, eager: true },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
